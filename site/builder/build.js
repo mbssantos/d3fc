a55313687e95f36972e3d4af1b5595026b0e1333
Ali Sheehan-Dare <alisd23@live.co.uk>
2016-08-12 16:23:11 +0100

    Add basic section and regex filtering of readmes

--- site/builder/build.js
+++ site/builder/build.js
@@ -1,8 +1,10 @@
 import fetchReadmes from './fetchReadmes';
 import parseReadmes from './parseReadmes';
+import filterReadmes from './filterReadmes';
 import yamlSerialize from './yamlSerialize';
 
 fetchReadmes()
   .then(parseReadmes)
+  .then(filterReadmes)
   .then(yamlSerialize)
   .catch(err => console.log(err));
